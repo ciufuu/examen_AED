1b591664fa998b2a9831a5d67056a014717fec34
ciufuu <laurentiuc2005@yahoo.com>
2026-01-15 16:39:04 +0200

    prelucrare date

--- proiect_examen.py
+++ proiect_examen.py
@@ -4,7 +4,7 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 from tabulate import tabulate 
 
-# ====== STATISTICI DESCRIPTIVE ======
+# ====== VERIFICAREA SI PRELUCRAREA SETULUI DE DATE ======
 
 df = pd.read_csv('apple_quality.csv')
 
@@ -12,9 +12,6 @@ print("\n=== Dimensiune ===")
 print(df.shape) #numarul de randuri si coloane
 print("\n=== Informații despre setul de date ===")
 print(df.info())
-stat = df.describe().T 
-print("\n" + " Statistici Descriptive ".center(104, "="))
-print(tabulate(stat, headers='keys', tablefmt='fancy_grid', numalign="right"))
 print("\n=== Valori duplicate ===")
 print(df.duplicated().sum()) 
 print("\n=== Valori lipsă ===")
@@ -23,7 +20,14 @@ if df.isna().sum().sum() == 0:
 else:   
     print(df.isna().sum())
 # Eliminare rand cu valoari lipsa
+print("\n=== Rânduri cu valori lipsă ===")
 print("\n" , df[df.isnull().any(axis=1)])
 df = df.drop(index=4000)
-print("\n" ,df.shape)
+print("\n Dimensiunea setului de date dupa prelucrare :" ,df.shape)
+
+df['Acidity'] = pd.to_numeric(df['Acidity']) # convertim coloana 'Acidity' de la object la numeric
+df['Quality_Binary'] = df['Quality'].map({'good': 1, 'bad': 0}) # adaugam o coloana binara pentru calitate
+stat = df.drop(columns=['Quality']).describe(include='all').T 
+print("\n" + " Statistici Descriptive ".center(104, "="))
+print(tabulate(stat, headers='keys', tablefmt='fancy_grid', numalign="right"))
 # %%
